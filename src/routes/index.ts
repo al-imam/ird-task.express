ed1a68b0b40d20111e165daf2e520b7bc0492d34
al-imam <alimam01828@gmail.com>
2024-01-31 19:01:03 +0600

    send only necessary data for navigation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,37 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/navigation", async (req, res) => {
   try {
-    const duas = await db.Category.findAll({ include: { all: true, nested: true } });
+    const duas = await db.Category.findAll({ 
+      include:[{ 
+        model: db.SubCategory, 
+        attributes: { exclude: ["subcat_name_bn"] }, 
+        include: [{ 
+          model: db.Dua, 
+          attributes: { 
+            exclude:[
+              "dua_name_bn",
+              "top_bn",
+              "top_en",
+              "dua_arabic",
+              "dua_indopak",
+              "clean_arabic",
+              "transliteration_bn",
+              "transliteration_en",
+              "translation_bn",
+              "translation_en",
+              "bottom_bn",
+              "bottom_en",
+              "refference_bn",
+              "refference_en",
+              "audio",
+            ], 
+          }, 
+        }], 
+      }],
+    });
+    
     res.json(duas.map((dua) => dua.toJSON()));
   } catch {
     res.status(500).send("Something went wrong");
